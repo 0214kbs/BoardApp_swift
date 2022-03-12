c7c46e3ea51b250872d21c850291769db723456f
bosunKwak <214kbs@gmail.com>
2022-03-12 20:02:13 +0900

    날짜 한글로 표시( ex) 2022년 03월 12일

--- BoardApp_swift/MemoListTableViewController.swift
+++ BoardApp_swift/MemoListTableViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class MemoListTableViewController: UITableViewController {
 
+    
+    let formatter: DateFormatter = {
+       let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .none
+        f.locale = Locale(identifier: "Ko_kr") //날짜를 한글로 표시
+        return f
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,19 +30,30 @@ class MemoListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
 
+    /*
+     <테이블 뷰 구현>
+     1. 테이블 뷰 배치 (table view controller를 사용할 경우, 자동 배치)
+     2. 프로토타입 셀 디자인 (cell style - subtitle로 변경)
+     3. 셀 아이덴티파이어 지정 (cell identifier를 cell로 지정)
+     4. 데이터 소스, 델리게이트 연결 (table view controller를 사용할 경우, 자동 연결)
+     5. 데이터 소스, 델리게이트 구현
+     */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
         return Memo.dummyMemoList.count
+    
     }
-
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        let target = Memo.dummyMemoList[indexPath.row]
-        cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = "\(target.insertDate)"
 
+        let target = Memo.dummyMemoList[indexPath.row]
+        // tableView에서 몇번째 셀인지 확인할 수 있음
+        
+        cell.textLabel?.text = target.content
+//        cell.detailTextLabel?.text = "\(target.insertDate)"
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
+        
         return cell
     }
 
