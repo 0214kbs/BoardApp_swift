675c5bea1ffa34388a283a50f9c7daae3c305f5b
bosunKwak <214kbs@gmail.com>
2022-03-12 20:26:05 +0900

    새 메모 쓰기

---
+++ BoardApp_swift/ComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ComposeViewController.swift
+//  BoardApp_swift
+//
+//  Created by 곽보선 on 2022/03/12.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
